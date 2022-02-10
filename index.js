36440d9fadeefab5e66392bccdcaa75ab686b642
calb <moesnerca22@spaldinghs.org>
2022-02-10 16:56:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,54 @@ const express = require("express");
 const { google } = require("googleapis");
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
-    const auth = new google.authGoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-    const client = await auth.getClient();
+app.post("/", async (req, res) => {
+  const { request, name } = req.body;
 
-    const googleSheets = google.sheets({version: "v4", auth: client});
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
 
-    const spreadsheetId = "1g6l3HL99CTEtF8rvT7Dz0gAf6K2xr0S-awCIa1V1SbQ";
+  // Create client instance for auth
+  const client = await auth.getClient();
 
-    const metaData = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId,
-    });
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
 
-    res.send(metaData);
+  const spreadsheetId = "1g6l3HL99CTEtF8rvT7Dz0gAf6K2xr0S-awCIa1V1SbQ";
+
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  // Read rows from spreadsheet
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:A",
+  });
+
+  // Write row(s) to spreadsheet
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:B",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[request, name]],
+    },
+  });
+
+  res.send("Successfully submitted! Thank you!");
 });
 
 app.listen(1337, (req, res) => console.log("running on 1337"));
